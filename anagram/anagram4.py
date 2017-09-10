2e5ab66e13f2f025f16f217a9f7f9e6f9c4e5af6
Jeremy Matson <thistuner@gmail.com>
2017-09-09 23:00:47 -0700

    Removed extraneous anagram scripts.
    Removed anagram2.py, anagram3.py, and anagram4.py.

--- anagram/anagram4.py
+++
@@ -1,5 +0,0 @@
-def detect_anagrams(string, array):
-    string = string.lower()
-    anagram = sorted(string)
-    candidates = (c for c in array if c.lower() != string)
-    return [c for c in candidates if sorted(c.lower()) == anagram]
