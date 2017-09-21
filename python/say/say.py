e815887bd2721bcbfbe41b1a3a6a6ecec2cfdfc4
J2Tuner <thistuner@gmail.com>
2017-09-20 22:01:36 -0700

    Updated say script.
    Made updates to say script, but it is still incomplete.

--- python/say/say.py
+++ python/say/say.py
@@ -1,2 +1,11 @@
-def say():
-    pass
+def say(n):
+    lowNumWords = ['zero', 'one', 'two', 'three', 'four',
+                   'five', 'six', 'seven', 'eight', 'nine',
+                   'ten', 'eleven', 'twelve', 'thirteen',
+                   'fourteen', 'fifteen', 'sixteen', 'seventeen',
+                   'eighteen', 'nineteen', 'twenty']
+
+    tensWords = [(30, 'thirty'), (40, 'forty'), (50, 'fifty'),
+                 (60, 'sixty'), (70, 'seventy'), (80, 'eighty'),
+                 (90, 'ninety'), (100, 'hundred'), (1000, 'thousand')]
+    magnitudes = []
