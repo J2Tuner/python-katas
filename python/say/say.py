2def4f74862fc28e3cbb15f84ae0ec5d5b76f529
J2Tuner <thistuner@gmail.com>
2017-09-22 06:43:24 -0700

    Updated say script.
    Included split_string function.

--- python/say/say.py
+++ python/say/say.py
@@ -8,4 +8,23 @@ def say(n):
     tensWords = [(30, 'thirty'), (40, 'forty'), (50, 'fifty'),
                  (60, 'sixty'), (70, 'seventy'), (80, 'eighty'),
                  (90, 'ninety'), (100, 'hundred'), (1000, 'thousand')]
-    magnitudes = []
+    
+    magnitudes = ['million', 'billion', 'trillion']
+
+    split_num = [x for x in str(n)]
+
+def split_string(string):
+    trimmedString = string
+    digitGroups = []
+    i = len(string) - 1
+    
+    while len(trimmedString) > 0:
+        stringLength = len(trimmedString)
+        print(trimmedString)
+        print(trimmedString[stringLength - 1: stringLength - 4 : -1])
+        digitGroups.append(trimmedString[stringLength - 1: stringLength - 4 : -1])
+        trimmedString = trimmedString[:stringLength - 3]
+
+    return digitGroups
+
+m = '123456789'
